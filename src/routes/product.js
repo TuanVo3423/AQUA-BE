ac9801cb4753891f2c26ed735a26cbe1dc333c05
TuanVo3423 <tuanvanvo2003@gmail.com>
2022-11-30 14:43:11 +0700

    add chat

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const productController = require('../App/Controller/ProductController');
+const express = require("express");
+const productController = require("../App/Controller/ProductController");
 const router = express.Router();
 
-router.post('/createPost',productController.createPost);
-router.get('/:id/edit',productController.editPost);
-router.delete('/:id',productController.deletePost);
-router.put('/:id',productController.updatePost);
+router.post("/createPost", productController.createPost);
+router.get("/shop", productController.getProductShop);
+router.get("/shop/filter", productController.filterProductShop);
+router.get("/:id/edit", productController.editPost);
+router.delete("/:id", productController.deletePost);
+router.put("/:id", productController.updatePost);
 
 module.exports = router;
