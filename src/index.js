f84cdc02d2feda86db3bebe6cfa7d65b13053fdd
TuanVo3423 <tuanvanvo2003@gmail.com>
2022-12-03 10:14:55 +0700

    loading message

--- src/index.js
+++ src/index.js
@@ -28,8 +28,8 @@ const io = new Server(server, {
       "https://aqua-fe.vercel.app",
       "https://aqua-admin-ochre.vercel.app",
     ],
-    methods: ["GET", "POST"],
     // origin: ["http://127.0.0.1:5173", "http://127.0.0.1:5174"],
+    methods: ["GET", "POST"],
   },
 });
 
